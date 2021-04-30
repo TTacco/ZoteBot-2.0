a0e8424cbbacfcc04bb33f282ee95c8531634617
BenZorb <uniforbenji@gmail.com>
2021-04-30 22:18:50 +1000

    Tentative Auto Delete functionality

---
+++ auto-delete.js
@@ -0,0 +1,10 @@
+client.on('message', message =>{
+if(client.message.channel === '837605332309377056'){
+	if(message.content === 'Dumb'){
+		message.author.send('I will literally kill you')
+	}
+	}
+});
+
+
+
