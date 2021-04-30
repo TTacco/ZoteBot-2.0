18e3c53d5f70e3dae470c6b16542a36877f5d9eb
TTacco <joventadem@gmail.com>
2021-04-30 13:05:27 +0800

    bare functionality

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-let = 'vegemite toast'
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('your-token-goes-here');
+
+
