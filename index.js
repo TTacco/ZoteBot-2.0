c36c8d0eab5c046ab951a2a84867a0b51a248861
TTacco <joventadem@gmail.com>
2021-04-30 15:11:41 +0800

    main renamed to index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('ZOTEBOT STATUS: ENABLED');
 });
 
 client.login(botTokenID);
